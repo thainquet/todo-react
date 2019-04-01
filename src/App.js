06c2ee09fc4d31e377265e372db225ca322f2549
thainq_uet <thainq00@gmail.com>
2019-04-01 09:27:43 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+//import css custom
 import './App.css';
+
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// define react router
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import CreateTodo from './components/create-todo.component'
+import EditTodo from './components/edit-todo.component'
+import TodoList from './components/todo-list.component'
+
+
+import Logo from './logo-tripi-64x30.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://tripi.vn" target="_blank" rel="noopener noreferrer">
+              <img src={Logo} width="64" height="30" alt="Tripi" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodoList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
